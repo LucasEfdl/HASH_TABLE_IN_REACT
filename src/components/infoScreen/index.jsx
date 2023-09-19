b7e1dff9c4514ffaca9409b66896541527711590
LucasEfdl <lucasefdl@gmail.com>
2023-09-19 16:42:36 -0300

    creation of InfoScreen component

---
+++ src/components/infoScreen/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css"
+
+export default function InfoScreen() {
+    return(
+        <div className={styles.wraper}>
+            <h1>Selecione um conato para mais informações</h1>
+    </div>
+    )
+}
